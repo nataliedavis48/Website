b2a28bbb26165e877be3172f40159f399ee59a9b
nataliedavis48 <natalie.davis48@gmail.com>
2026-03-09 12:21:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -163,6 +163,32 @@ function showArticle(index) {
   const article = articles[index];
   let textWithVocab = article.text;
 
+  if (article.vocab) {
+    Object.keys(article.vocab).forEach(word => {
+      const regex = new RegExp(`\\b${word}\\b`, "gi");
+      textWithVocab = textWithVocab.replace(
+        regex,
+        `<span class="vocab-word" data-word="${word}">${word}</span>`
+      );
+    });
+  }
+
+  articleDisplay.innerHTML = `
+    <h3>${article.title}</h3>
+    <p>${textWithVocab}</p>
+    <div id="vocab-info" class="vocab-box">Click a highlighted word to see the meaning.</div>
+  `;
+
+  const vocabWords = document.querySelectorAll(".vocab-word");
+  const vocabInfo = document.getElementById("vocab-info");
+
+  vocabWords.forEach(item => {
+    item.addEventListener("click", () => {
+      const word = item.dataset.word;
+      vocabInfo.innerHTML = `<strong>${word}</strong>: ${article.vocab[word]}`;
+    });
+  });
+}
   Object.keys(article.vocab).forEach(word => {
     const regex = new RegExp(`\\b${word}\\b`, "gi");
     textWithVocab = textWithVocab.replace(
